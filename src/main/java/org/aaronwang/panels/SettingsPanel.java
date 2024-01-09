9269eed692fb55ada1794fbd59ca45b542c9456f
Aaron Wang <aaronw4932@gmail.com>
2024-01-09 13:54:49 -0500

    work on settingsd panelsdfjdslkgjn

--- src/main/java/org/aaronwang/panels/SettingsPanel.java
+++ src/main/java/org/aaronwang/panels/SettingsPanel.java
@@ -3,6 +3,7 @@ package org.aaronwang.panels;
 import org.aaronwang.UI.GradientPanel;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -10,11 +11,61 @@ public class SettingsPanel extends GradientPanel implements FocusListener {
     public SettingsPanel(ActionListener gui) {
         super(new Color(0x3d137f), new Color(0x342d40), GradientPanel.VERTICAL_FILL);
         this.setPreferredSize(new Dimension(1000, 624));
-        JLabel label = new JLabel("placeholder", JLabel.CENTER);
-        label.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 48));
-        this.setBackground(new Color(0xD9D9D9));
-        this.setLayout(new BorderLayout());
-        this.add(label, BorderLayout.CENTER);
+        this.setLayout(null);
+
+        JLabel title = new JLabel("Settings", JLabel.CENTER);
+        title.setForeground(new Color(0xcdc2db)); // Secondary
+        title.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 48));
+        title.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createLineBorder(new Color(0x4b4358), 8, true), // Secondary container
+                "MicroMacro",
+                TitledBorder.LEFT,
+                TitledBorder.TOP,
+                new Font("Century Gothic", Font.BOLD | Font.ITALIC, 26),
+                new Color(0xe9def8))); // On secondary container
+
+        JLabel delayLabel = new JLabel("<html><u>Recording delay (ms)</u></html>", JLabel.CENTER);
+        delayLabel.setForeground(new Color(0xf0b7c5)); // Tertiary
+        delayLabel.setBackground(new Color(0x643b46)); // Tertiary container
+        delayLabel.setFont(new Font("Century Gothic", Font.BOLD, 20));
+
+        JTextField recordDelay = new JTextField();
+        recordDelay.setFont(new Font("Century Gothic", Font.BOLD, 14));
+
+
+        JLabel keybindLabel = new JLabel("<html><u>Preferred Recording Keybind</u></html>", JLabel.CENTER);
+        keybindLabel.setForeground(new Color(0xf0b7c5)); // Tertiary
+        keybindLabel.setBackground(new Color(0x643b46)); // Tertiary container
+        keybindLabel.setFont(new Font("Century Gothic", Font.BOLD, 20));
+
+        JLabel themeLabel = new JLabel("<html><u>Preferred Theme</u></html>", JLabel.CENTER);
+        themeLabel.setForeground(new Color(0xf0b7c5)); // Tertiary
+        themeLabel.setBackground(new Color(0x643b46)); // Tertiary container
+        themeLabel.setFont(new Font("Century Gothic", Font.BOLD, 20));
+
+        JLabel hintsLabel = new JLabel("<html><u>Show Startup Hints</u></html>", JLabel.CENTER);
+        hintsLabel.setForeground(new Color(0xf0b7c5)); // Tertiary
+        hintsLabel.setBackground(new Color(0x643b46)); // Tertiary container
+        hintsLabel.setFont(new Font("Century Gothic", Font.BOLD, 20));
+
+
+
+        this.add(title);
+        title.setBounds(370, 20, 260, 110);
+
+        this.add(delayLabel);
+        delayLabel.setBounds(110, 189, 212, 50);
+        this.add(recordDelay);
+        recordDelay.setBounds(790, 189, 100, 35);
+
+        this.add(keybindLabel);
+        keybindLabel.setBounds(110, 298, 281, 50);
+
+        this.add(themeLabel);
+        themeLabel.setBounds(110, 407, 160, 50);
+
+        this.add(hintsLabel);
+        hintsLabel.setBounds(110, 516, 177, 50);
     }
 
     @Override
